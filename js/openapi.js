015ba6fe38d7481598465661593b1c89fd20f833
Valeria <valeria.cabrerapacheco18@gmail.com>
2026-06-07 20:29:35 -0400

    added open api page

---
+++ js/openapi.js
@@ -0,0 +1,40 @@
+const imageBtn = document.getElementById("dogImageBtn");
+const dogBtn = document.getElementById("dogBtn");
+const output = document.getElementById("output");
+
+// Show Random Dog Image
+imageBtn.addEventListener("click", function () {
+
+    fetch("https://api.thedogapi.com/v1/images/search")
+
+        .then(response => response.json())
+        .then(data => {
+            output.innerHTML = `
+                <h2>Random Dog</h2>
+                <img src="${data[0].url}" width="300">
+            `;
+        })
+
+        .catch(error => {
+            output.innerHTML = "<p>Error loading dog image.</p>";
+            console.log(error);
+        });
+});
+
+// Dog Info 
+dogBtn.addEventListener("click", function () {
+    fetch("https://api.thedogapi.com/v1/images/search?has_breeds=1")
+
+    .then(response => response.json())
+    .then(data => {
+        output.innerHTML = `
+        <h2>Dog Information</h2>
+        <p><strong>ID:</strong> ${data[0].id}
+        <p><strong>Height:</strong> ${data[0].height}`;
+    })
+    
+    .catch(error => {
+        output.innerHTML = "<p>Error loading breed information.</p>";
+        console.log(error);
+    });
+});
