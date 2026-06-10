d80b366523a36fff8a596ce6110fda117c7651d5
Valeria <valeria.cabrerapacheco18@gmail.com>
2026-06-09 22:29:58 -0400

    The Dog API breed was not consistently returning breed data, so I updated the Dog Info section to display data that is returned by the API

--- js/openapi.js
+++ js/openapi.js
@@ -11,7 +11,7 @@ imageBtn.addEventListener("click", function () {
         .then(data => {
             output.innerHTML = `
                 <h2>Random Dog</h2>
-                <img src="${data[0].url}" width="300">
+                <img src="${data[0].url}" alt="Random Dog Image" width="300">
             `;
         })
 
@@ -23,14 +23,20 @@ imageBtn.addEventListener("click", function () {
 
 // Dog Info 
 dogBtn.addEventListener("click", function () {
-    fetch("https://api.thedogapi.com/v1/images/search?has_breeds=1")
+    fetch("https://api.thedogapi.com/v1/images/search?has_breeds=1", {
+    headers: {
+        "x-api-key": "live_n90m0DCnPIhkb2sDe7ZlcAVqTJBnoQ72kpRJLar0Oi3A1iKuwplO5ostjdJSWWGr"}
+    })
 
     .then(response => response.json())
     .then(data => {
+        console.log(data);
+// theres no breeds so I added the information available (id, width, height)
         output.innerHTML = `
         <h2>Dog Information</h2>
-        <p><strong>ID:</strong> ${data[0].id}
-        <p><strong>Height:</strong> ${data[0].height}`;
+        <p><strong>ID:</strong> ${data[0].id}</p>
+        <p><strong>Width:</strong> ${data[0].width}</p>
+        <p><strong>Height:</strong> ${data[0].height}</p>`;
     })
     
     .catch(error => {
